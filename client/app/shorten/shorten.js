5d2c1b0373657fd3de669a2bbf958cadd7bff55b
Gerrit <gerrityntema@gmail.com>
2016-02-13 20:40:54 -0800

    Fix shortened link routes

--- client/app/shorten/shorten.js
+++ client/app/shorten/shorten.js
@@ -6,9 +6,11 @@ angular.module('shortly.shorten', [])
   $scope.error;
 
   $scope.addLink = function () {
+  	$scope.loading = true;
     Links.addOne({url: $scope.link.url})
     .then(function (resp) {
-    	$location.path('/');
+    	$scope.loading = false;
+      $location.path('/');
     });
   };
 });
